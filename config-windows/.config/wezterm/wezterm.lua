2363e1a5837cf6b9cc3a217c54b85ad6f5424f25
sid-6581 <sid6581@gmail.com>
2024-08-14 16:48:39 -0500

    fix: disable custom block glyphs in wezterm

--- config-windows/.config/wezterm/wezterm.lua
+++ config-windows/.config/wezterm/wezterm.lua
@@ -17,6 +17,7 @@ config.default_cursor_style = "SteadyBar"
 config.freetype_load_flags = "DEFAULT"
 config.max_fps = 240
 config.animation_fps = 240
+config.custom_block_glyphs = false
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
 config.font_size = 9.5
 config.command_palette_font_size = 12
